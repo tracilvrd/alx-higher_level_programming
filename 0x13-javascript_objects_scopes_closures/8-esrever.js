657ea75894e5a5169c7b00dd963fb07371b09c81
tracilvrd <oghenekparobo.ese@gmail.com>
2023-03-14 22:30:49 +0100

    JS Warmup

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    return list.reduceRight(function (array, count) {
-	array.push(count);
+    return list.reduceRight(function (array, current) {
+	array.push(current);
 	return array;
     }, []);
 };
