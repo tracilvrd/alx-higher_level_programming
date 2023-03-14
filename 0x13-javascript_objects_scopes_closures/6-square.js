1d4bbb68e7305611878f2464f4f2ede0be937347
tracilvrd <oghenekparobo.ese@gmail.com>
2023-03-14 23:20:00 +0100

    JS Warmup

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,11 +2,11 @@
 module.exports = class Square extends require('./5-square.js') {
     charPrint (c) {
 	if (c === undefined) {
-	    this.print();
+	  this.print();
 	} else {
-	    for (let i = 0; i < this.height; i++) {
+	  for (let i = 0; i < this.height; i++) {
 		console.log(c.repeat(this.width));
-	    }
+	   }
 	}
     }
 };
