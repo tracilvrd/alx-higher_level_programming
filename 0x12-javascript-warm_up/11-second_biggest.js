e8c11d53b262319d19a32ba726e5dbf7d0bb6b5e
tracilvrd <oghenekparobo.ese@gmail.com>
2023-03-14 02:56:08 +0100

    JS

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 const nums = args.map(arg => parseInt(arg)).filter(num => !isNaN(num)).sort((a, b) => b - a);
 
-if (nums.length == 0 || nums.length == 1) {
+if (nums.length === 0 || nums.length === 1) {
   console.log(0);
 } else {
   console.log(nums[1]);
